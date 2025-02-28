37e9e9fb46ac8b2f4a5e43c87bf65a62a47603f7
ayushstwt <ayusrivastava2k3@gmail.com>
2025-02-28 10:47:06 +0530

    project setup

---
+++ src/main/java/com/ayshriv/linkmindbackend/LinkmindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayshriv.linkmindbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkmindBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkmindBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayshriv/linkmindbackend/LinkmindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayshriv.linkmindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkmindBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
